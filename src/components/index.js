a9d8ac2fb69ab550e0ec66eb7be58effc5818840
Tevin-creator <isaactevin67@gmail..com>
2021-12-07 02:17:52 +0300

    renamed MainContent folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 export {default as Navbar} from './Navbar/Navbar';
 export {default as MainContent} from './MainContent/MainContent';
-
 export {default as Orders} from './Orders/Orders';
 export {default as Add} from './Add/Add';
-
 export {default as Footer} from './Footer/Footer';
